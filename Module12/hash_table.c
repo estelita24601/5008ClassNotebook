415e772b78235912fc2a38c6576d6a4c66063c75
Estelita <estelitac24401@hotmail.com>
2024-04-01 13:32:30 -0700

    expanded driver to demonstrate print and find functions for hash map

--- Module12/hash_table.c
+++ Module12/hash_table.c
@@ -4,7 +4,7 @@ int main()
 {
     int table_size = 100;
     HashTable table = create_hash_table(table_size);
-    printf("created table of size %d\n\n", table_size);
+    printf("created table of size %d\n", table_size);
 
     add(table, table_size, "Darth Vader", "Anakin Skywalker");
     print_debug("added an item to hash table");
@@ -17,6 +17,35 @@ int main()
 
     add(table, table_size, "Old Ben", "Obi-Wan Kenobi");
     print_debug("added an item to hash table");
+
+    add(table, table_size, "Voldemort", "Tom Marvolo Riddle");
+    print_debug("added an item to hash table");
+
+    add(table, table_size, "The Boy Who Lived", "Harry Potter");
+    print_debug("added an item to hash table");
+
+    add(table, table_size, "Captain Proton", "Tom Paris");
+    print_debug("added an item to hash table");
+
+    add(table, table_size, "007", "James Bond");
+    print_debug("added an item to hash table");
+
+    print_hash_table(table, table_size);
+
+    char *key = "Old Ben";
+    printf("trying to find value from key \"%s\":\n", key);
+    char *found_value = table_get(table, table_size, key);
+    printf("\tvalue = \"%s\"\n", found_value);
+
+    key = "captain proton";
+    printf("trying to find value from key \"%s\":\n", key);
+    found_value = table_get(table, table_size, key);
+    printf("\tvalue = \"%s\"\n", found_value);
+
+    key = "Captain Proton";
+    printf("trying to find value from key \"%s\":\n", key);
+    found_value = table_get(table, table_size, key);
+    printf("\tvalue = \"%s\"\n", found_value);
 
     free_table(table, table_size);
     return 0;
